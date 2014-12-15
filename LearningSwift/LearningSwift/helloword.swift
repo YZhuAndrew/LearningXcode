305247da4db5026d58570c2a3bbb037652210c10
Zhu Yun <yzhu.andrew@163.com>
2014-12-15 21:38:55 +0800

    create a xcode workspace
    create a new Xcode workspace which named LearningXcode, and create a
    new project named LearningSwift.

---
+++ LearningSwift/LearningSwift/helloword.swift
@@ -0,0 +1,9 @@
+//
+//  helloword.swift
+//  LearningSwift
+//
+//  Created by 朱溳 on 14/12/15.
+//  Copyright (c) 2014年 溳 朱. All rights reserved.
+//
+
+import Foundation
---
+++ LearningSwift/LearningSwift/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  LearningSwift
+//
+//  Created by 朱溳 on 14/12/15.
+//  Copyright (c) 2014年 溳 朱. All rights reserved.
+//
+
+import Foundation
+
+println("Hello, World!")
+
